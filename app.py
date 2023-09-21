9b60a46e8fd7b4e98797f9d4c1152550b16e87df
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-09-22 00:35:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,38 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, jsonify
 from pytube import YouTube
-import os
+import json
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/audio/', methods=['GET'])
+def download_audio():
+    link = request.args.get('link')
 
-@app.route('/download', methods=['POST'])
-def download():
-    url = request.form['url']
-    yt = YouTube(url)
-    
-    # Get the title and size of the video
-    title = yt.title
-    size = yt.streams.get_audio_only().filesize
-    
-    # Download the audio
-    audio_stream = yt.streams.filter(only_audio=True).first()
-    audio_stream.download(output_path='downloads', filename='audio')
-    
-    return f'Title: {title}<br>Size: {size} bytes<br><a href="/downloads/audio.mp4" download>Download Audio</a>'
+    if not link:
+        return jsonify({'error': 'Invalid or missing "link" parameter'}), 400
+
+    try:
+        utube = YouTube(link)
+        Title = utube.title
+        Thumbnail = utube.thumbnail_url
+
+        ados = utube.streams.filter(only_audio=True)
+        resolution_list = [ado.abr for ado in ados]
+        itag_list = [ado.itag for ado in ados]
+
+        dwnld_link_list = [ados.get_by_itag(i).url for i in itag_list]
+
+        data_set = {
+            'audio_title': Title,
+            'audio_thumbnail': Thumbnail,
+            'audio_resolution_list': resolution_list,
+            'audio_download_link_list': dwnld_link_list
+        }
+
+        return jsonify(data_set)
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    
+    app.run()
